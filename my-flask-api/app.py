c36124ccccb7eace28f4655c0c308f904126971e
kevinagustino11082003@gmail.com <kevinagustino11082003@gmail.com>
2025-07-01 18:37:51 +0700

    fix: port and Procfile

--- my-flask-api/app.py
+++ my-flask-api/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -10,6 +11,11 @@ def submit():
         "data": data
     })
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
-    return "Server is running!"
+    return "API is running"
+
+# Wajib ini agar bisa jalan di Railway:
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
